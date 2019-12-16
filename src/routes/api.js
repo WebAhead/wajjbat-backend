e2a8f3c2e4cf37aac302af19c2f6b0df340aa4c5
mohamadD <rediab88@gmail.com>
2019-12-16 13:13:30 +0200

    add the request changes

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { businesses, businessesId, newReview } = require("../controllers/api");
-const { aw3 } = require("../controllers/aw3");
+const { s3Controller } = require("../controllers/s3Controller");
 
 router.post("/businesses", businesses);
 
@@ -10,6 +10,6 @@ router.get("/businesses/:id", businessesId);
 router.post("/new-review", newReview);
 
 // router that handel aw3 requests
-router.get("/sign-s3", aw3);
+router.get("/sign-s3", s3Controller);
 
 module.exports = router;
