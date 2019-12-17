26dc210ef82f44d431b31ab1f0b660396bcb8682
mohamadD <rediab88@gmail.com>
2019-12-17 11:56:16 +0200

    The name of the function is aw3 not s3Controller . i just fixed the name of the function .

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { businesses, businessesId, newReview } = require("../controllers/api");
-const { s3Controller } = require("../controllers/s3Controller");
+const { aw3 } = require("../controllers/s3Controller");
 
 router.post("/businesses", businesses);
 
@@ -10,6 +10,6 @@ router.get("/businesses/:id", businessesId);
 router.post("/new-review", newReview);
 
 // router that handel aw3 requests
-router.get("/sign-s3", s3Controller);
+router.get("/sign-s3", aw3);
 
 module.exports = router;
