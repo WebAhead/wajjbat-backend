bf974d7da05430570e535eff60210b1ec2586742
Mario <mario.saliba98@gmail.com>
2019-12-17 14:10:48 +0200

    fixes the profile image

--- src/queries/addNewUser.js
+++ src/queries/addNewUser.js
@@ -1,6 +1,6 @@
 const db = require("../database/db_connection");
 
-module.exports.addNewUser = (firstName, lastName, email, profile_image) =>
+module.exports.addNewUser = (firstName, lastName, email, profileImage) =>
     db.query(` 
     INSERT INTO users(first_name,last_name,email,profile_image)
     VALUES($1,$2,$3,$4)`,
