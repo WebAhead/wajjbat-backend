47a2903e18befcae263ec2338f3d792fc897e9ee
mohamadD <rediab88@gmail.com>
2019-12-17 15:28:38 +0200

    I changed the query to handle the approval / dis approval option closes #26

--- src/queries/editBusinesById.js
+++ src/queries/editBusinesById.js
@@ -1,10 +1,21 @@
 const db = require("../database/db_connection");
 
-const editBusinesById = data =>
+const editBusinesById = data => {
+  // if user approve the business we will get 'true' from the crm system and turn it to approved
+  // if user disapprove the business we will get 'no' from the crm system and turn it to disapprove
+  // if user doesnt choosed any option we keep the prev value
+
+  let approved = data.approved;
+  if (data.approved === true) {
+    approved = "approved";
+  } else if (data.approved === false) {
+    approved = "disapproved";
+  }
+
   db.query(
     `UPDATE businesses SET
-    name=$1, phone=$2, email=$3,description=$4,cuisine=$5,address=$6,lat=$7,lng=$8,business_type=$9
-      WHERE id=$10`,
+    name=$1, phone=$2, email=$3,description=$4,cuisine=$5,address=$6,lat=$7,lng=$8,business_type=$9,approved=$10
+      WHERE id=$11`,
     [
       data.name,
       data.phone,
@@ -15,8 +26,10 @@ const editBusinesById = data =>
       data.lat,
       data.lng,
       data.business_type,
+      approved,
       data.id
     ]
   );
+};
 
 module.exports = { editBusinesById };
