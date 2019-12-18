a3d514202df22c6e843fff194e9cf69ac999600f
mohamadD <rediab88@gmail.com>
2019-12-18 12:41:58 +0200

    fixes the aws problem

--- src/queries/editBusinesById.js
+++ src/queries/editBusinesById.js
@@ -4,7 +4,6 @@ const editBusinesById = data => {
   // if user approve the business we will get 'true' from the crm system and turn it to approved
   // if user disapprove the business we will get 'no' from the crm system and turn it to disapprove
   // if user doesnt choosed any option we keep the prev value
-
   let approved = data.approved;
   if (data.approved === true) {
     approved = "approved";
@@ -12,7 +11,14 @@ const editBusinesById = data => {
     approved = "disapproved";
   }
 
-  db.query(
+  if (!data.name) {
+    return db.query("UPDATE businesses SET approved=$1 WHERE id=$2", [
+      approved,
+      data.id
+    ]);
+  }
+
+  return db.query(
     `UPDATE businesses SET
     name=$1, phone=$2, email=$3,description=$4,cuisine=$5,address=$6,lat=$7,lng=$8,business_type=$9,approved=$10
       WHERE id=$11`,
