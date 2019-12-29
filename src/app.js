528e083d1a07097d10287ad85e907a5168c3e362
Mario <mario.saliba98@gmail.com>
2019-12-29 12:41:29 +0200

    .

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(cors({
   optionsSuccessStatus: 200,
   credentials: true,
-  origin: 'http://localhost:3000'
+  origin: process.env.ORIGIN
 }));
 // initialize passport
 
