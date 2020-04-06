1e0f2c5343484da64a54c66da2d82175a24206a1
Mario Saliba <mario.saliba98@gmail.com>
2020-04-06 10:21:38 +0300

    Update social.js

--- src/routes/social.js
+++ src/routes/social.js
@@ -4,23 +4,23 @@ const social = require('../controllers/social');
 import middleware from '../middleware';
 
 router.get('/allPosts', social.getAllPosts);
-router.get('/nPosts', middleware.verifyToken,social.getNPosts);
-router.get('/comments', middleware.verifyToken, social.getComments);
+router.get('/nPosts', middleware.verifyToken(false), social.getNPosts);
+router.get('/comments', middleware.verifyToken(false), social.getComments);
 router.get('/latestComment', social.getLatestComment);
 router.get('/likes', social.getLikes);
-router.get('/userPosts', middleware.verifyToken, social.getAllUserPosts);
-router.get('/userFavorites', middleware.verifyToken, social.getAllUserFavorites);
-router.get('/followers', middleware.verifyToken, social.getFollowers);
+router.get('/userPosts', middleware.verifyToken(false), social.getAllUserPosts);
+router.get('/userFavorites', middleware.verifyToken(false), social.getAllUserFavorites);
+router.get('/followers', middleware.verifyToken(false), social.getFollowers);
 
-router.post('/addPost', middleware.verifyToken, social.addPost);
-router.post('/favorites', middleware.verifyToken, social.favoritePosts);
-router.post('/followers', middleware.verifyToken, social.followers);
-router.post('/likes', middleware.verifyToken, social.likes);
-router.post('/comment', middleware.verifyToken, social.comment);
+router.post('/addPost', middleware.verifyToken(false), social.addPost);
+router.post('/favorites', middleware.verifyToken(false), social.favoritePosts);
+router.post('/followers', middleware.verifyToken(false), social.followers);
+router.post('/likes', middleware.verifyToken(false), social.likes);
+router.post('/comment', middleware.verifyToken(false), social.comment);
 
-router.delete('/deletePost', middleware.verifyToken, social.deletePost);
-router.delete('/unLike', middleware.verifyToken, social.unLike);
-router.delete('/unFollow', middleware.verifyToken, social.unFollow);
-router.delete('/unFavorite', middleware.verifyToken, social.unFavorite);
+router.delete('/deletePost', middleware.verifyToken(false), social.deletePost);
+router.delete('/unLike', middleware.verifyToken(false), social.unLike);
+router.delete('/unFollow', middleware.verifyToken(false), social.unFollow);
+router.delete('/unFavorite', middleware.verifyToken(false), social.unFavorite);
 
 module.exports = router;
